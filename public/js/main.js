8ae0742bb6b54c23a12dd65d4620a84e159d8aa8
denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
2014-01-29 21:30:23 +0200

    Add search

--- public/js/main.js
+++ public/js/main.js
@@ -12,3 +12,89 @@ $('#export-img').on('click', function(e){
     });
 });
 
+var possibleTargets = ['other', 'navbar', 'toolbar', 'tertiary', 'secondary', 'narrow', 'wide', 'single']
+    , activeTargets = []
+    , tempHTML = Handlebars.compile($("#dummy-html").html())
+    ;
+
+//  Расположение определяется степенью двойки:
+//  0 - overlay (исключение)
+//  1 - single (primary)
+//  2 - wide (primary)
+//  4 - narrow (primary)
+//  8 - secondary
+//  16 - tertiary
+//  32 - toolbar
+//  64 - navbar
+//  128 - other
+
+var parseTargets = function(targets) {
+    var result = [];
+    for (var mask = 128, i = 0; mask > 0; mask >>= 1, i++) {
+        if (mask & targets) {
+            result.push(possibleTargets[i]);
+        }
+    }
+    return result;
+};
+
+Handlebars.registerHelper('editor', function(context, options) {
+    var wrap = '';
+
+    for(var i=0, j=context.length; i<j; i++) {
+        wrap += "<span>" + options.fn(context[i]) + "</span>";
+    }
+
+    return wrap;
+});
+
+$(document).ready(function(){
+
+    $(".lego_toggler").on("click", ".lego_toggle_i", function(){
+        $(this).parent().children().each( function() {
+            $(this).toggleClass("__active")
+        });
+        $('.lego_search-result').toggleClass("__list");
+    });
+
+    $(".lego_search-result").on("click", ".lego_search-result_h", function(){
+        $(this).parent().toggleClass("__closed");
+    });
+
+    $(".lego_layer").on("click", ".editable", function(){
+        $(this).append(tempHTML());
+    });
+
+    $(".lego_layer").on("click", ".editor_x", function(e){
+        e.stopImmediatePropagation();
+        $(this).parent().remove();
+    });
+
+    $(".lego_search-result").on("click", ".lego_search-result_i", function(e){
+        e.preventDefault();
+        if (activeTargets.length) {
+            for (var i = 0; i < activeTargets.length; i++) {
+                activeTargets[i].removeClass('editable');
+            }
+            activeTargets = [];
+        }
+
+        var targets = $(this)[0].dataset.target
+            , results = (targets == 0) ? 0 : parseTargets(targets)
+            ;
+
+        if (!results) {
+            var overlay = $('.lego_layer');
+            overlay.addClass('editable');
+            activeTargets.push(overlay);
+        } else {
+            for (var j = 0; j < results.length; j++) {
+                var elem = $('[data-target="' + results[j] + '"]');
+                activeTargets.push(elem);
+                elem.addClass('editable');
+            }
+        }
+    });
+
+});
+
---
+++ public/js/search.js
@@ -0,0 +1,105 @@
+/**
+ * Created by denis.przendzinskis on 29/01/14.
+ */
+
+// GLOBAL
+var parsedTree = {}
+    , parsed = false
+    , template = Handlebars.compile($("#search-result-list").html());
+// /GLOBAL
+
+var filterValidSpecs = function(pagesTree) {
+
+    var cat
+        , base = pagesTree["base"]
+        , project = pagesTree["project"]
+    ;
+
+    for (spec in base) {
+
+        if ((base[spec]["specFile"] != undefined)
+            && (base[spec]["specFile"]["category"] != undefined)) {
+            cat = base[spec]["specFile"]["category"];
+            if (!parsedTree[cat]) parsedTree[cat] = {};
+            parsedTree[cat][spec] = base[spec]["specFile"];
+            delete parsedTree[cat][spec]["category"];
+        }
+    }
+
+    for (spec in project) {
+        if ((project[spec]["specFile"] != undefined)
+            && (project[spec]["specFile"]["category"] != undefined)) {
+            cat = project[spec]["specFile"]["category"];
+            if (!parsedTree[cat]) parsedTree[cat] = {};
+            parsedTree[cat][spec] = project[spec]["specFile"];
+            delete parsedTree[cat][spec]["category"];
+        }
+    }
+    parsed = true;
+    $("#lego_search-result").html(template(parsedTree));
+}
+
+$.ajax({
+    url: 'http://localhost:8080/data/pages_tree.json',
+    success: function(data) {
+        filterValidSpecs(data);
+    }
+});
+
+var fuzzySearch = function(q, allData) {
+    var result = {}
+        , query = q.toLowerCase().trim()
+        , qRegExp = new RegExp(query)
+        , lowerCat
+    ;
+
+    for (cat in allData) {
+        lowerCat = cat.toLowerCase();
+
+         // if query matches current category, all category's articles are considered a match
+        if (qRegExp.test(lowerCat.match(query))) {
+            result[cat] = allData[cat];
+            continue;
+        }
+
+        // otherwise, continue parsing
+        for (spec in allData[cat]) {
+
+            var info = (allData[cat][spec]["info"] != undefined) ? allData[cat][spec]["info"].toLocaleLowerCase() : '';
+            var keywords = (allData[cat][spec]["keywords"] != undefined) ? allData[cat][spec]["keywords"].toLocaleLowerCase() : '';
+            var title = (allData[cat][spec]["title"] != undefined) ? allData[cat][spec]["title"].toLocaleLowerCase() : '';
+
+            if (qRegExp.test(info.match(query)) ||
+                qRegExp.test(keywords.match(query)) ||
+                qRegExp.test(title.match(query))) {
+
+                if (!result[cat]) result[cat] = {};
+                result[cat][spec] = allData[cat][spec];
+            }
+        }
+    }
+    return result;
+}
+
+$('#search').on("keyup", function(){
+
+    var value = $(this).val();
+
+    if (parsed) {
+        var result = fuzzySearch(value, parsedTree);
+        if ($.isEmptyObject(result)) {
+            $("#lego_search-result").html("Ничего не найдено");
+        } else {
+            $("#lego_search-result").html(template(result));
+        }
+    } else {
+        $("#lego_search-result").html("Байты не готовы!!!");
+    }
+});
+
+
+
+
+
+
+
