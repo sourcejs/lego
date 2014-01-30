63c80ce90a1911c8f6abbe4550b7252250ae1a53
ivan.metelev <ivan.metelev@odnoklassniki.ru>
2014-01-30 17:49:00 +0400

    get html variations

--- public/js/modifiers.js
+++ public/js/modifiers.js
@@ -9,6 +9,9 @@ var modifiers = (function() {
 		$.ajax({
 			url: "http://127.0.0.1:8080/api",
 			type: 'POST',
+			data: {
+				task: 'parseModifiers'
+			},
 			dataType: 'json',
 			success: function(data) {
 				allModifiers = $.extend({}, data, true);
@@ -18,29 +21,32 @@ var modifiers = (function() {
 	}
 
 	function getHTMLpart( activeElement, callback ) {
-		// Передаю url спеки, получаю объект спеки
-		$.ajax({
-			url: "http://127.0.0.1:8080/api",
-			data: {
+		var callback = callback || function() {};
 
+		$.ajax({
+			url: 'http://127.0.0.1:8080/api',
+			data: {
+				specID: activeElement.specFileUrl.substr(1)
 			},
 			type: 'POST',
 			dataType: 'json',
 			success: function(data) {
-
-				// Sections
-				for (var sectionName in data.sections) {
-					console.log('mod tpl ' + sectionName);
-				}
-
-				// Project className
-				activeElement.baseClass = data.className;
+				callback(data);
 			}
+		});
 
-		})
 	}
 
 	function searchInBlock( activeElement ) {
+console.log('search');
+		var $wrap = $('.js-modificators .lego_form-i'),
+			template = '<div class="lego_form-i_w"> \
+						<label class="lego_form-i_txt"> \
+							<input class="lego_checkbox" type="checkbox" name="modificators"/> \
+						</label> \
+					</div>';
+
+		$wrap.empty;
 
 		var allSelectors = activeElement.node.querySelectorAll('[class]');
 		for (var currentSelector = 0; currentSelector < allSelectors; currentSelector++) {
@@ -49,8 +55,53 @@ var modifiers = (function() {
 			for (var curClassList = 0; curClassList < currentSelectorClassList.length; curClassList++) {
 
 				if ( currentSelectorClassList[curClassList].indexOf(activeElement.baseClass) ) {
-					console.log('mod css ' + activeElement.baseClass + ' ' + currentSelectorClassList[curClassList]);
+					//console.log('mod css ' + activeElement.baseClass + ' ' + currentSelectorClassList[curClassList]);
+
+					if ((allModifiers[currentSelectorClassList[curClassList]]) && (allModifiers[currentSelectorClassList[curClassList]].length)) {
+
+						for (var currentModifier = 0; currentModifier < allModifiers[currentSelectorClassList[curClassList]].length; currentModifier++) {
+							var $template = $(template);
+							$template.find('input')
+								.attr('data-elem', currentSelectorClassList[curClassList])
+								.attr('data-mod', allModifiers[currentSelectorClassList[curClassList]][currentModifier]);
+
+							$template.find('label').append(allModifiers[currentSelectorClassList[curClassList]][currentModifier]);
+
+						}
+					}
+
+
 				}
+			}
+		}
+	}
+
+	function searchVariations( data ) {
+		var $wrap = $('.js-variations .lego_form-i'),
+			template = '<div class="lego_form-i_w"> \
+						<label class="lego_form-i_txt"> \
+							<input class="lego_checkbox" type="radio" name="variations"/> \
+						</label> \
+					</div>';
+
+		$wrap.empty;
+
+		// Sections
+		for (var sectionIndex = 0; sectionIndex < data.sections.length; sectionIndex++) {
+			for (var sectionName in data.sections[sectionIndex]) {
+				//console.log('mod tpl ' + sectionName);
+
+				var $template = $(template);
+				$template.find('input')
+					.attr('data-elem', data.url)
+					.attr('data-mod', data.sections[sectionIndex][sectionName]);
+
+				if (sectionIndex == 0) {
+					$template.find('input').prop('checked', true);
+				}
+
+				$template.find('label').append(sectionName);
+				$wrap.append($template);
 			}
 		}
 	}
@@ -67,11 +118,17 @@ var modifiers = (function() {
 		lookForHTMLMod: function( selector ) {
 			activeElement = {};
 
-			activeElement.node = document.querySelector();
-			activeElement.specFileUrl = activeElement.node.getAttribute('');
+			activeElement.node = selector;
+			activeElement.specFileUrl = activeElement.node.getAttribute('data-url');
 
-			getHTMLpart(activeElement, function() {
-				searchInBlock( activeElement );
+			getHTMLpart(activeElement, function(data) {
+
+				searchVariations(data);
+
+				// Project className
+				activeElement.baseClass = data.className;
+
+				//searchInBlock( activeElement );
 			})
 
 			return this;
@@ -80,12 +137,24 @@ var modifiers = (function() {
 		lookForCSSMod: function() {
 			if ( !allModifiers ) {
 				setTimeout(function() {
-					getCSSMod( modifiers.lookForCSSMod() )
+					getCSSMod( function() {
+						modifiers.lookForCSSMod();
+					} )
 				}, 1000)
+				return this;
 			}
+
+			console.log(allModifiers);
 
 			return this;
 		}
 	}
 
 })()
+
+var r = document.createElement('div');
+r.setAttribute('data-current', 'true');
+r.setAttribute('data-url', '/base/footer');
+document.body.appendChild(r);
+
+modifiers.lookForHTMLMod(  document.querySelector('[data-url]') );
